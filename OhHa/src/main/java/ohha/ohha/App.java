fdd8ae4840c21f7cd46f6d80dee4ab85e3bcbb82
unknown <tuomas.lehtinen@helsinki.fi>
2014-01-18 00:35:55 +0200

    muokattu 18.01.14

--- OhHa/src/main/java/ohha/ohha/App.java
+++ OhHa/src/main/java/ohha/ohha/App.java
@@ -1,13 +1,11 @@
 package ohha.ohha;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        testi t = new testi();
+
+        t.run();
+
     }
 }
