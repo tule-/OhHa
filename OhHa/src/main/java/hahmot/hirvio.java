8b0fb1f36744c38a90a16357e8a20e1f04285f5d
unknown <tuomas.lehtinen@helsinki.fi>
2014-01-18 13:52:44 +0200

    muokattu 18.01.14

---
+++ OhHa/src/main/java/hahmot/hirvio.java
@@ -0,0 +1,30 @@
+package hahmot;
+
+import java.awt.Graphics;
+import java.util.Random;
+import logiikka.hahmo;
+
+public class hirvio extends hahmo {
+    
+    public hirvio(){
+        super(0,0);
+    }
+    
+    @Override
+    public void siirra(int dx, int dy){
+        
+        Random r = new Random();
+        dx = r.nextInt(3) - 1;
+        dy = r.nextInt(3) - 1;
+        
+        if(this.getX() + dx <= 19 && this.getX() + dx >= 0 && this.getY() + dy <= 9 && this.getY() + dy >= 0){
+            super.siirra(dx, dy);
+        }
+    }
+
+    @Override
+    public void piirra(Graphics graphics) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- OhHa/src/main/java/ohha/ohha/testi.java
+++ OhHa/src/main/java/ohha/ohha/testi.java
@@ -1,5 +1,6 @@
 package ohha.ohha;
 
+import hahmot.hirvio;
 import hahmot.pelaaja;
 import java.util.Scanner;
 import logiikka.hahmo;
@@ -7,19 +8,25 @@ import logiikka.hahmo;
 public class testi {
 
     private hahmo p;
+    private hahmo h;
     private Scanner l;
 
     public testi() {
         this.p = new pelaaja();
+        this.h = new hirvio();
         this.l = new Scanner(System.in);
     }
 
     public void run() {
+        
+        //PELIKENTTÄ
         while (true) {
             for (int i = 0; i < 10; i++) {
                 for (int j = 0; j < 20; j++) {
                     if (p.getX() == j && p.getY() == i) {
                         System.out.print("O");
+                    }else if (h.getX() == j && h.getY() == i){
+                        System.out.print("H");
                     }else{
                         System.out.print("-");
                     }
@@ -27,6 +34,7 @@ public class testi {
                 System.out.println("");
             }
             
+            //PELAAJAN LIIKE
             String k = l.nextLine();
             if(k.equals("d")){
                 p.siirra(1, 0);
@@ -43,6 +51,13 @@ public class testi {
             if(k.equals("x")){
                 break;
             }
+            
+            //HIRVIÖN LIIKE. (0,0) KOSKA LIIKE ARVOTAAN ITSE METODISSA
+            h.siirra(0, 0);
+            
+            //KOORDINAATIT
+            System.out.println("pelaaja: " +  p.getX() + " " + p.getY());
+            System.out.println("hirvio: " +  h.getX() + " " + h.getY());
         }
     }
 }
