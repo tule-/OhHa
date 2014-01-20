c1cf479664290d5d55fbd731eef86ea1c7a0e37c
unknown <tuomas.lehtinen@helsinki.fi>
2014-01-20 14:40:21 +0200

    muokattu 20.01.14

--- OhHa/src/test/java/hahmot/pelaajaTest.java
+++ OhHa/src/test/java/hahmot/pelaajaTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class pelaajaTest {
+
     private hahmo p;
 
     @Before
@@ -17,38 +18,43 @@ public class pelaajaTest {
         //ALKU KOORDINAATIT (10,5)
     }
 
-    
     //TULEE MUUTTAA KUN super:IA MUUTETAAN
     @Test
     public void pelaajallaAloitusKoordinaatit() {
         assertEquals(10, p.getX());
         assertEquals(5, p.getY());
     }
-    
+
     @Test
-    public void pelaajaLiikkuuOikein(){
+    public void pelaajaLiikkuuOikein() {
         p.siirra(1, 1);
-        
+
         assertEquals(11, p.getX());
         assertEquals(6, p.getY());
     }
-    
-    
+
     //TULEE MUUTTAA KUN PELIKENTÄN SUURUUS MUUTTUU
     @Test
-    public void pelaajaPysyyPelikentanSisalla(){
+    public void pelaajaPysyyPelikentanSisalla() {
         p.siirra(1000, 0);
         p.siirra(0, 1000);
-        p.siirra(-1000, 0);
-        p.siirra(0, -1000);
-        
+
         assertEquals(10, p.getX());
         assertEquals(5, p.getY());
     }
-    
+
+    @Test
+    public void pelaajaPysyyPelikentanSisalla2() {
+        p.siirra(-1000, 0);
+        p.siirra(0, -1000);
+
+        assertEquals(10, p.getX());
+        assertEquals(5, p.getY());
+    }
+
     //LISÄÄ TESTEJÄ KUN GRAFIIKKALIITTYMÄ SAADAAN KASAAN
     @Test
-    public void piirraMetodi(){
+    public void piirraMetodi() {
         p.piirra(null);
     }
 }
