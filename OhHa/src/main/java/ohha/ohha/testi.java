40184dd04964469fa1d456eaa4f17590a1bbe32e
unknown <tuomas.lehtinen@helsinki.fi>
2014-01-20 22:42:11 +0200

    muokattu 20.01.14

--- OhHa/src/main/java/ohha/ohha/testi.java
+++ OhHa/src/main/java/ohha/ohha/testi.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import logiikka.hahmo;
 import logiikka.hirvioGeneraattori;
 import logiikka.kuolema;
+import logiikka.voimakentta;
 
 public class testi {
 
@@ -20,27 +21,12 @@ public class testi {
     public void run() {
         Scanner l = new Scanner(System.in);
         kuolema ku = new kuolema();
+        voimakentta vk = new voimakentta();
 
         //HIRVIÖIDEN LKM (+1)
         hg.lisaa(5);
 
-        //PELIKENTTÄ
         while (true) {
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 20; j++) {
-                    for (hirvio h1 : hg.getLista()) {
-                        if(h1.getX() == j && h1.getY() == i){
-                            System.out.print("H");
-                        }
-                    }
-                    if(p.getX() == j && p.getY() == i){
-                        System.out.print("O");
-                    }else{
-                        System.out.print("-");
-                    }
-                }
-                System.out.println("");
-            }
 
             //PELAAJAN LIIKE
             String k = l.nextLine();
@@ -56,6 +42,9 @@ public class testi {
             if (k.equals("w")) {
                 p.siirra(0, -1);
             }
+            if (k.equals("e")) {
+                vk.hyokkaa(p.getX(), p.getY());
+            }
             if (k.equals("x")) {
                 break;
             }
@@ -65,6 +54,30 @@ public class testi {
                 h1.siirra(0, 0);
             }
 
+            //KOORDINAATIT
+            System.out.println("pelaaja: " + p.getX() + " " + p.getY());
+
+            //PELIKENTTÄ
+            for (int i = 0; i < 10; i++) {
+
+                for (int j = 0; j < 20; j++) {
+
+                    for (hirvio h1 : hg.getLista()) {
+                        if (h1.getX() == j && h1.getY() == i) {
+                            System.out.print("H");
+                        }
+                    }
+                    if (p.getX() == j && p.getY() == i) {
+                        System.out.print("O");
+                    }else if(vk.getX1() == j && vk.getY1() == i || vk.getX2() == j && vk.getY2() == i || vk.getX3() == j && vk.getY3() == i || vk.getX4() == j && vk.getY4() == i){
+                        System.out.print("X");
+                    }else {
+                        System.out.print("-");
+                    }
+                }
+                System.out.println("");
+            }
+            
             //TARKISTUS ONKO PELAAJA ELOSSA
             //(koordinaatit täytyy viedä luokkaan jotta tarkistus toimii)
             int apu1 = 0;
@@ -78,12 +91,9 @@ public class testi {
             if (apu1 == 1) {
                 break;
             }
-
-            //KOORDINAATIT
-            System.out.println("pelaaja: " + p.getX() + " " + p.getY());
-            for (hirvio h1 : hg.getLista()) {
-                System.out.println("hirvio: " + h1.getX() + " " + h1.getY());
-            }
+            
+            //VOIMAKENTÄN NOLLAUS
+            vk.nollaa();
         }
     }
 }
